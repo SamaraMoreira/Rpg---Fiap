327ad934dca34d08035ce4de264c181f374cfc7c
SamaraMoreira <samaramoreira000@gmail.com>
2024-03-07 11:57:20 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Rpg___Fiap.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character Kinigth = new Character();
+        [HttpGet]
+        public ActionResult<Character> get()
+        {
+            return Ok(Kinigth);
+        }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,15 @@
+﻿namespace Rpg___Fiap.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "Froddo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int intelligence { get; set; } = 10;
+
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
